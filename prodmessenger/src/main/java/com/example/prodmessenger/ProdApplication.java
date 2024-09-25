8b5f4a714c416ff3acdfb420946ee7e7d1e13875
João Pedro Batista da silva <116274892+Joaopedrobs7@users.noreply.github.com>
2024-09-25 19:19:44 -0300

    Update ProdApplication.java

--- prodmessenger/src/main/java/com/example/prodmessenger/ProdApplication.java
+++ prodmessenger/src/main/java/com/example/prodmessenger/ProdApplication.java
@@ -1,6 +1,6 @@
 package com.example.prodmessenger;
 
-import org.springframework.amqp.core.DirectExchange;
+//import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class ProdApplication {
 
-	 static final String directExchangeName = "direct-exchange";
+	 //static final String directExchangeName = "direct-exchange";
 	 static final String topicExchangeName = "topic-exchange";
 	 static final String routingKeyLow = "rota-um";
 	 static final String routingKeyMid = "rota-dois";
