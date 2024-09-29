ccc65512b5fc6582c5f8b83d253b405e05380ac6
João Pedro Batista da silva <116274892+Joaopedrobs7@users.noreply.github.com>
2024-09-28 21:41:24 -0300

    Update Runner.java

--- prodmessenger/src/main/java/com/example/prodmessenger/Runner.java
+++ prodmessenger/src/main/java/com/example/prodmessenger/Runner.java
@@ -21,19 +21,41 @@ public class Runner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
     	Scanner ler = new Scanner(System.in);
-        
+        String msg1 = " [Perfil Baixo risco] $BTC +2.5% = $63,611";
+        String msg2 = "[Perfil Medio risco] $DOT +5.0% = $4.92";
+        String msg3 = "[Perfil Alto risco] $XRD -10.0% = $0.02049";
+    	int choice;
+    	String routingKey = "";
     	while(true) {
-    		System.out.println("Digite a mensagem:");
-   		    String msg;
-   		    msg = ler.nextLine(); 
+    		//pra onde vai
+    		
+    		System.out.println("1-[BAIXO]\n2-[MEDIO]\n3-[AlTO]\n");
+    		//System.out.println("Digite a mensagem:");
+   		    String msg = "";
+   		    //msg = ler.nextLine(); 
+   		    choice = ler.nextInt();
+   		    
+   		    if(choice == 1) {
+   		    	routingKey = ProdApplication.routingKeyLow;
+   		    	msg = msg1;
+   		    }
+   		    else if(choice == 2) {
+   		    	routingKey = ProdApplication.routingKeyMid;
+   		    	msg = msg2;
+   		    }
+   		    else if (choice == 3) {
+   		    	routingKey = ProdApplication.routingKeyHigh;
+   		    	msg = msg3;
+   		    }
+   		    
    		    
    		    if(msg.contains("sair"))
    		    	break;
    		    
 
     		//rabbitTemplate.convertAndSend(ProdApplication.directExchangeName, ProdApplication.routingKey, msg);
-   		    rabbitTemplate.convertAndSend(ProdApplication.topicExchangeName, ProdApplication.routingKeyLow, msg);
-   		    //se for do tipo 1 manda para a rota um assim por diante
+   		    rabbitTemplate.convertAndSend(ProdApplication.topicExchangeName, routingKey, msg);
+   		    
 
     	}
         context.close();
